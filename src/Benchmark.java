f2da30781bcf2eb2f4ddc2b405baa09fdbb0207c
Bill Lee <lyhmail2000@126.com>
2023-03-22 19:18:46 +0800

    refining benchmark

--- src/Benchmark.java
+++ src/Benchmark.java
@@ -3,50 +3,72 @@ import java.util.concurrent.TimeUnit;
 
 public class Benchmark {
     public static final int
-            ONE_THREAD = 1_0000_0000,
-            SAMPLE_COUNT = 1000000,
-            THREADS_COUNT = 4,
-            SIMPLE_RESERVOIR_THREAD = THREADS_COUNT * ONE_THREAD;
+            ONE_THREAD = 100_0000,
+            SAMPLE_COUNT = 1000,
+            THREADS_COUNT = 8,
+            SIMPLE_RESERVOIR_THREAD = THREADS_COUNT * ONE_THREAD,
+            TEST_COUNT = 100;
 
     public static void main(String[] args) {
 
-        //testSimpleReservoir();
+        bench("SEQUENTIAL RESERVOIR", testSimpleReservoir);
 
         System.out.println("------------PARTITION-------------");
 
-        testParallelReservoir();
+        bench("PARALLEL RESERVOIR", testParallelReservoir);
     }
 
-    static void testSimpleReservoir() {
+    static void bench(String name, Testable toTest){
+        System.out.println("testing " + name + ", running it for " + TEST_COUNT + " times");
+        long totalTime = 0, maxTime = Long.MIN_VALUE, minTime = Long.MAX_VALUE;
+        for (int i = 0; i < TEST_COUNT; i++){
+            long time = toTest.test();
+            maxTime = Long.max(maxTime, time);
+            minTime = Long.min(minTime, time);
+            totalTime += time;
+        }
+
+        System.out.println("Average time to run " + name + " is " + totalTime / (double)(TEST_COUNT)+"ms");
+        System.out.println("\t with max elapsed time = " + maxTime + "ms; min elapsed time = " + minTime+"ms");
+    }
+
+
+
+    static Testable testSimpleReservoir = ()-> {
 
         //RandomUtility rand = new RandomUtility();
 
         SimpleReservoir<Integer> simpleReservoir = new SimpleReservoir<>(SAMPLE_COUNT);
 
-        System.out.println("expected total = " + SIMPLE_RESERVOIR_THREAD);
+        // System.out.println("expected total = " + SIMPLE_RESERVOIR_THREAD);
 
         long startTime = System.nanoTime();
-        System.out.println("SEQUENTIAL Sampling started");
+        // System.out.println("SEQUENTIAL Sampling started");
 
         for (int i = 0; i < SIMPLE_RESERVOIR_THREAD; i++)
             simpleReservoir.trySample(i);
 
         SampleResult<Integer> sampleResult = simpleReservoir.getSampleResult();
         long endTime = System.nanoTime();
-        System.out.println("SEQUENTIAL Sampling completed, using " + TimeUnit.NANOSECONDS.toMillis(endTime - startTime) + "ms");
+        long timeElapsed = TimeUnit.NANOSECONDS.toMillis(endTime - startTime);
+        /*
+        System.out.println("SEQUENTIAL Sampling completed, using " + timeElapsed + "ms");
 
         System.out.println("sample result: \n" + sampleResult.samples + "\n" +
                 "\twith actual total = " + sampleResult.total);
-    }
+         */
 
-    static void testParallelReservoir() {
-        System.out.println("expected total = " + ONE_THREAD * THREADS_COUNT);
+        return timeElapsed;
+    };
+
+    static Testable testParallelReservoir = ()->{
+        // System.out.println("expected total = " + ONE_THREAD * THREADS_COUNT);
 
         ParallelReservoir<Integer> parallelReservoir =
                 new ParallelReservoir<>(SAMPLE_COUNT);
 
         long startTime = System.nanoTime();
-        System.out.println("PARALLEL Sampling started");
+        // System.out.println("PARALLEL Sampling started");
 
         ArrayList<Thread> handles = new ArrayList<>(THREADS_COUNT);
 
@@ -73,10 +95,18 @@ public class Benchmark {
 
         SampleResult<Integer> sampleResult = parallelReservoir.getSampleResult();
         long endTime = System.nanoTime();
-        System.out.println("PARALLEL Sampling completed, using " + TimeUnit.NANOSECONDS.toMillis(endTime - startTime) + "ms");
+        long timeElapsed = TimeUnit.NANOSECONDS.toMillis(endTime - startTime);
+        /*
+        System.out.println("PARALLEL Sampling completed, using " + timeElapsed + "ms");
 
         System.out.println("sample result: \n" + sampleResult.samples + "\n" +
                 "\twith actual total = " + sampleResult.total);
 
-    }
+         */
+        return timeElapsed;
+    };
+}
+
+interface Testable{
+    long test();
 }
